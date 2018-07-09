e24679849dde9c777e3797c1a94f8d7064cef810
at-peter <atrazhev@ualberta.ca>
2018-07-09 19:37:14 +0000

    Tried to get textures.

--- feature_detection/opencv_orb.py
+++ feature_detection/opencv_orb.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 
-img = cv.imread('depthmap.jpg', 0)
+img = cv.imread('./images/tx2_camera/Left.png', 0)
 #Orb initialization
 orb = cv.ORB_create(nfeatures=10000, scoreType = cv.ORB_FAST_SCORE)
 #keypoints
@@ -11,4 +11,11 @@ kp = orb.detect(img,None)
 kp, des = orb.compute(img, kp)
 
 img2 =cv.drawKeypoints(img, kp, 0,color=(0,255,0), flags = 0)
-plt.imshow(img2), plt.show()
+
+
+
+while 1:
+	cv.imshow('ORB', img2) 
+	if cv.waitKey(1) == 0x1b:
+		print('ESC pressed. Exiting ...')
+		break

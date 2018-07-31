0305e644d0e6a5713e31ae41b9aa966fff77abe7
Peter <atrazhev@ualberta.ca>
2018-07-31 14:42:21 -0600

    Trying out the C++ api. Added a test script to test camera functionality on liza's comp.

--- feature_detection/bruteForceMatching.py
+++ feature_detection/bruteForceMatching.py
@@ -20,14 +20,14 @@ kp2, des2 = orb.detectAndCompute(img2, None)
 
 bf = cv.BFMatcher_create(cv.NORM_HAMMING, crossCheck = True)
 
-matches = bf.knnMatch(np.asarray(des1[1], np.float32), np.asarray(des2[1], np.float32))
+matches = bf.match(des1, des2)
 
 #ort them in order of distance:
 matches = sorted(matches, key = lambda x:x.distance)
 
 #draw only the first 10 matches
 
-img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:100] ,None ,flags=2)
+img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[100:] ,None ,flags=2)
 im3s = cv.resize(img3, (960,540))
 
 cv.imwrite("BFmatch_DM1toW3_knn.png",img3)
